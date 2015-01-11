718be9c3904dd89b2f7188a00e13fccb578f79ac
Jeremi Stadler <stadler.jeremi@gmail.com>
2015-01-11 22:46:19 +0100

    Add foothold hit collition

--- build/js/main.js
+++ build/js/main.js
@@ -26,6 +26,9 @@ var Vector = (function () {
     Vector.plus = function (v1, v2) {
         return new Vector(v1.x + v2.x, v1.y + v2.y);
     };
+    Vector.plusSize = function (v1, v2) {
+        return new Vector(v1.x + v2.width, v1.y + v2.height);
+    };
     Vector.dot = function (v1, v2) {
         return v1.x * v2.x + v1.y * v2.y;
     };
@@ -52,8 +55,11 @@ var Foothold = (function () {
         this.Size = Size;
     }
     Foothold.prototype.draw = function (ctx) {
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+        ctx.fillStyle = this.playerTouches ? 'rgba(100, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.2)';
         ctx.fillRect(this.Position.x, this.Position.y, this.Size.width, this.Size.height);
+    };
+    Foothold.prototype.isPointColliding = function (point, velocity) {
+        return (this.Position.x <= point.x && this.Position.x + this.Size.width >= point.x && this.Position.y >= point.y && this.Position.y <= point.y + velocity.y && true);
     };
     return Foothold;
 })();
@@ -64,6 +70,7 @@ var KeyCodes;
     KeyCodes[KeyCodes["up"] = 38] = "up";
     KeyCodes[KeyCodes["down"] = 40] = "down";
     KeyCodes[KeyCodes["enter"] = 13] = "enter";
+    KeyCodes[KeyCodes["space"] = 32] = "space";
 })(KeyCodes || (KeyCodes = {}));
 var Texture = (function () {
     function Texture(path) {
@@ -75,17 +82,17 @@ var Texture = (function () {
     }
     Texture.prototype.draw = function (ctx, pos, size) {
         if (this.hasError) {
-            size = size || new Vector(100, 100);
+            size = size || new Size(100, 100);
             ctx.fillStyle = 'rgba(200, 0, 0, 0.2)';
-            ctx.fillRect(pos.x, pos.y, size.x, size.y);
+            ctx.fillRect(pos.x, pos.y, size.width, size.height);
             ctx.fillStyle = 'rgb(0, 0, 0)';
             ctx.fillText('error...', pos.x + 10, pos.y + 10);
             return;
         }
         if (!this.hasLoaded) {
-            size = size || new Vector(100, 100);
+            size = size || new Size(100, 100);
             ctx.fillStyle = 'rgba(0, 0, 200, 0.2)';
-            ctx.fillRect(pos.x, pos.y, size.x, size.y);
+            ctx.fillRect(pos.x, pos.y, size.width, size.height);
             ctx.fillStyle = 'rgb(0, 0, 0)';
             ctx.fillText('loading...', pos.x, pos.y);
             return;
@@ -93,7 +100,7 @@ var Texture = (function () {
         if (size == null)
             ctx.drawImage(this.image, pos.x, pos.y, this.image.width, this.image.height);
         else
-            ctx.drawImage(this.image, pos.x, pos.y, size.x, size.y);
+            ctx.drawImage(this.image, pos.x, pos.y, size.width, size.height);
     };
     return Texture;
 })();
@@ -106,7 +113,7 @@ var Camera = (function () {
     };
     Camera.prototype.update = function () {
         var targetPos = new Vector(0, 0);
-        targetPos.x = player.Position.x + -game.canvas.width / 2 - player.Size.x / 2;
+        targetPos.x = player.Position.x + -game.canvas.width / 2 - player.Size.width / 2;
         //this.Position = targetPos;
         game.ctx.setTransform(1, 0, 0, 1, 0, 0);
         game.ctx.translate(this.Position.x, this.Position.y);
@@ -120,23 +127,31 @@ var Player = (function () {
     function Player() {
     }
     Player.prototype.init = function () {
-        this.Position = new Vector(0, 0);
+        this.Position = new Vector(500, 0);
         this.Velocity = new Vector(0, 0);
-        this.Size = new Vector(60, 80);
+        this.Size = new Size(60, 80);
         this.image = new Texture('http://nxcache.nexon.net/spotlight/112/007kn-7e9ea6e9-e3c1-402e-803d-7df82ad5ac53.gif');
+        this.hasJumped = true;
         var instance = this;
         window.onkeydown = function (e) { return instance.onKeyDown(e); };
         window.onkeyup = function (e) { return instance.onKeyUp(e); };
     };
     Player.prototype.onKeyDown = function (e) {
         if (e.keyCode == 37 /* left */)
-            this.Velocity.x = -1;
+            this.Velocity.x = -3;
         else if (e.keyCode == 39 /* right */)
-            this.Velocity.x = 1;
+            this.Velocity.x = 3;
         if (e.keyCode == 40 /* down */) {
-            debugger;
+            this.Position.y++;
+            this.Velocity.y++;
         }
         if (e.keyCode == 38 /* up */) {
+        }
+        if (e.keyCode == 32 /* space */) {
+            if (this.hasJumped == false) {
+                this.Velocity.y -= 8;
+                this.hasJumped = true;
+            }
         }
     };
     Player.prototype.onKeyUp = function (e) {
@@ -146,10 +161,32 @@ var Player = (function () {
             this.Velocity.x = 0;
     };
     Player.prototype.update = function () {
+        this.Velocity.y += 0.3;
+        for (var i = 0; i < map.Footholds.length; i++)
+            map.Footholds[i].playerTouches = false;
+        for (var i = 0; i < map.Footholds.length; i++) {
+            if (map.Footholds[i].isPointColliding(Vector.plus(this.Position, new Vector(-this.Size.width / 2, 0)), this.Velocity) || map.Footholds[i].isPointColliding(Vector.plus(this.Position, new Vector(this.Size.width / 2, 0)), this.Velocity)) {
+                map.Footholds[i].playerTouches = true;
+                this.Velocity.y = 0;
+                this.Position.y = map.Footholds[i].Position.y;
+                this.hasJumped = false;
+            }
+        }
         this.Position = Vector.plus(this.Position, this.Velocity);
+        if (this.Position.y > 1000) {
+            this.Position.y = 0;
+            this.Velocity.y = 0;
+        }
     };
     Player.prototype.draw = function () {
-        this.image.draw(game.ctx, this.Position, this.Size);
+        this.image.draw(game.ctx, new Vector(this.Position.x - this.Size.width / 2, this.Position.y - this.Size.height), this.Size);
+        game.ctx.beginPath();
+        game.ctx.strokeStyle = "black";
+        game.ctx.moveTo(this.Position.x - 5, this.Position.y);
+        game.ctx.lineTo(this.Position.x + 5, this.Position.y);
+        game.ctx.moveTo(this.Position.x, this.Position.y + 5);
+        game.ctx.lineTo(this.Position.x, this.Position.y - 5);
+        game.ctx.stroke();
     };
     return Player;
 })();
@@ -160,7 +197,7 @@ var World = (function () {
         this.Background = new Texture('http://fc09.deviantart.net/fs25/f/2008/086/e/e/Render__Henesys_by_iChicken.png');
         this.Footholds = [];
         for (var i = 0; i < 10; i++)
-            this.Footholds.push(new Foothold(new Vector(Math.random() * 800, Math.random() * 800), new Size(Math.random() * 500, 100)));
+            this.Footholds.push(new Foothold(new Vector(Math.random() * 1400, Math.random() * 1000), new Size(Math.random() * 500 + 200, 40)));
     };
     World.prototype.update = function () {
     };
--- scripts/main.ts
+++ scripts/main.ts
@@ -10,7 +10,8 @@ class Vector {
 	constructor(public x: number, public y: number) { }
 	static times(k: number, v: Vector) { return new Vector(k * v.x, k * v.y); }
 	static minus(v1: Vector, v2: Vector) { return new Vector(v1.x - v2.x, v1.y - v2.y); }
-	static plus(v1: Vector, v2: Vector) { return new Vector(v1.x + v2.x, v1.y + v2.y); }
+    static plus(v1: Vector, v2: Vector) { return new Vector(v1.x + v2.x, v1.y + v2.y); }
+    static plusSize(v1: Vector, v2: Size) { return new Vector(v1.x + v2.width, v1.y + v2.height); }
 	static dot(v1: Vector, v2: Vector) { return v1.x * v2.x + v1.y * v2.y; }
 	static mag(v: Vector) { return Math.sqrt(v.x * v.x + v.y * v.y); }
 	static norm(v: Vector) {
@@ -24,12 +25,23 @@ class Vector {
 }
 
 class Foothold {
-	constructor(public Position : Vector, public Size : Size){ }
-	
+    playerTouches: boolean;
+
+    constructor(public Position: Vector, public Size: Size) { }
+
 	draw(ctx : CanvasRenderingContext2D){
-		ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+        ctx.fillStyle = this.playerTouches ? 'rgba(100, 0, 0, 0.2)':'rgba(0, 0, 0, 0.2)';
 		ctx.fillRect(this.Position.x, this.Position.y, this.Size.width, this.Size.height);
-	}
+    }
+
+    isPointColliding(point: Vector, velocity: Vector): boolean {
+        return (
+            this.Position.x <= point.x &&
+            this.Position.x + this.Size.width >= point.x &&
+            this.Position.y >= point.y &&
+            this.Position.y <= point.y + velocity.y &&
+            true);
+    }
 }
 
 enum KeyCodes { 
@@ -37,7 +49,8 @@ enum KeyCodes {
 	right = 39,
 	up = 38,
 	down = 40,
-	enter = 13
+    enter = 13,
+    space = 32
  }
 
 class Texture {
@@ -54,31 +67,29 @@ class Texture {
 		this.image.onerror = () => instance.hasError = true;
 	}
 	
-	draw(ctx : CanvasRenderingContext2D, pos : Vector, size? : Vector) {
+	draw(ctx : CanvasRenderingContext2D, pos : Vector, size? : Size) {
 		if (this.hasError){
-			size = size || new Vector(100, 100);
+			size = size || new Size(100, 100);
 			ctx.fillStyle = 'rgba(200, 0, 0, 0.2)';
-			ctx.fillRect(pos.x, pos.y, size.x, size.y);
+			ctx.fillRect(pos.x, pos.y, size.width, size.height);
 			ctx.fillStyle = 'rgb(0, 0, 0)';
 			ctx.fillText('error...', pos.x + 10, pos.y + 10);
 			return;
 		}
 		
 		if (!this.hasLoaded){
-			size = size || new Vector(100, 100);
+            size = size || new Size(100, 100);
 			ctx.fillStyle = 'rgba(0, 0, 200, 0.2)';
-			ctx.fillRect(pos.x, pos.y, size.x, size.y);
+            ctx.fillRect(pos.x, pos.y, size.width, size.height);
 			ctx.fillStyle = 'rgb(0, 0, 0)';
 			ctx.fillText('loading...', pos.x, pos.y);
 			return;
 		}
 		
 		if (size == null)
-			ctx.drawImage(this.image, pos.x, pos.y, 
-						  this.image.width, this.image.height);	
+			ctx.drawImage(this.image, pos.x, pos.y, this.image.width, this.image.height);	
 		else
-			ctx.drawImage(this.image, pos.x, pos.y, 
-						  size.x, size.y);	
+			ctx.drawImage(this.image, pos.x, pos.y, size.width, size.height);	
 	}
 }
 
@@ -91,7 +102,7 @@ class Camera {
 	}
 	update() {
 		var targetPos = new Vector(0, 0);
-		targetPos.x = player.Position.x + -game.canvas.width / 2 - player.Size.x / 2;
+		targetPos.x = player.Position.x + -game.canvas.width / 2 - player.Size.width / 2;
 		
 		//this.Position = targetPos;
 		
@@ -106,14 +117,16 @@ class Player {
 	image : Texture;
 	Position : Vector;
 	Velocity : Vector;
-	Size : Vector;
+    Size: Size;
+    hasJumped: boolean;
 	
 	init() {
-		this.Position = new Vector(0, 0);
+		this.Position = new Vector(500, 0);
 		this.Velocity = new Vector(0, 0);
-		this.Size = new Vector(60, 80);
+        this.Size = new Size(60, 80);
 		this.image = new Texture('http://nxcache.nexon.net/spotlight/112/007kn-7e9ea6e9-e3c1-402e-803d-7df82ad5ac53.gif');
-	
+        this.hasJumped = true;
+
 		var instance = this;
 		window.onkeydown = (e) => instance.onKeyDown(e);
 		window.onkeyup = (e) => instance.onKeyUp(e);
@@ -121,12 +134,18 @@ class Player {
 	
 	onKeyDown(e : KeyboardEvent){
 		if (e.keyCode == KeyCodes.left)
-			this.Velocity.x = -1;
+			this.Velocity.x = -3;
 		else if (e.keyCode == KeyCodes.right)
-			this.Velocity.x = 1;
+			this.Velocity.x = 3;
 		  
-		if (e.keyCode == KeyCodes.down) { debugger; }
-		if (e.keyCode == KeyCodes.up) { }
+        if (e.keyCode == KeyCodes.down) { this.Position.y++; this.Velocity.y++; }
+        if (e.keyCode == KeyCodes.up) { }
+        if (e.keyCode == KeyCodes.space) {
+            if (this.hasJumped == false) {
+                this.Velocity.y -= 8;
+                this.hasJumped = true;
+            }
+        }
 	}
 	
 	onKeyUp(e : KeyboardEvent){
@@ -136,12 +155,40 @@ class Player {
 			this.Velocity.x = 0;
 	}
 	
-	update() {
-		this.Position = Vector.plus(this.Position, this.Velocity);
-	}
+    update() {
+        this.Velocity.y += 0.3;
+
+        for (var i = 0; i < map.Footholds.length; i++)
+            map.Footholds[i].playerTouches = false;
+        
+         
+        for (var i = 0; i < map.Footholds.length; i++) {
+            if (map.Footholds[i].isPointColliding(Vector.plus(this.Position, new Vector(-this.Size.width / 2, 0)), this.Velocity) ||
+                map.Footholds[i].isPointColliding(Vector.plus(this.Position, new Vector(this.Size.width / 2, 0)), this.Velocity)) {
+                map.Footholds[i].playerTouches = true;
+                this.Velocity.y = 0;
+                this.Position.y = map.Footholds[i].Position.y;
+                this.hasJumped = false;
+            }
+        }
+
+        this.Position = Vector.plus(this.Position, this.Velocity);
+
+        if (this.Position.y > 1000) {
+            this.Position.y = 0;
+            this.Velocity.y = 0;
+        }
+    }
 	
 	draw() {
-		this.image.draw(game.ctx, this.Position, this.Size);
+        this.image.draw(game.ctx, new Vector(this.Position.x - this.Size.width / 2, this.Position.y - this.Size.height), this.Size);
+        game.ctx.beginPath();
+        game.ctx.strokeStyle = "black";
+        game.ctx.moveTo(this.Position.x - 5, this.Position.y);
+        game.ctx.lineTo(this.Position.x + 5, this.Position.y);
+        game.ctx.moveTo(this.Position.x, this.Position.y + 5);
+        game.ctx.lineTo(this.Position.x, this.Position.y - 5);
+        game.ctx.stroke();
 	}
 }
 
@@ -151,12 +198,12 @@ class World {
 	
 	init() {
 		this.Background = new Texture('http://fc09.deviantart.net/fs25/f/2008/086/e/e/Render__Henesys_by_iChicken.png');
-	
+	    
 		this.Footholds = [];
 		for (var i = 0; i < 10; i++)
 			this.Footholds.push(new Foothold(
-				new Vector(Math.random() * 800, Math.random() * 800), 
-				new Size(Math.random() * 500, 100)))
+				new Vector(Math.random() * 1400, Math.random() * 1000), 
+				new Size(Math.random() * 500 + 200, 40)))
 	}
 	update() { }
 	draw() {
@@ -192,7 +239,6 @@ class Game {
 		player.draw();
 	}
 }
-
 
 var game = new Game();
 var camera = new Camera();
