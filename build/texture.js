7050fa7c3ba11a995bd01dd0cb09d68e3b55359a
Jeremi Stadler <stadler.jeremi@gmail.com>
2015-01-18 01:19:48 +0100

    comment out debug lines

--- build/texture.js
+++ build/texture.js
@@ -10,30 +10,30 @@ var Texture = (function () {
         this.image.onerror = function () { return instance.hasError = true; };
     }
     Texture.prototype.draw = function (ctx, pos, size) {
-        if (this.hasError) {
-            size = size || new Size(100, 100);
-            ctx.fillStyle = 'rgba(200, 0, 0, 0.2)';
-            ctx.fillRect(pos.x, pos.y, size.width, size.height);
-            ctx.fillStyle = 'rgb(0, 0, 0)';
-            ctx.fillText('error...', pos.x + 10, pos.y + 10);
-            return;
-        }
-        if (!this.hasLoaded) {
-            size = size || new Size(100, 100);
-            ctx.fillStyle = 'rgba(0, 0, 200, 0.2)';
-            ctx.fillRect(pos.x, pos.y, size.width, size.height);
-            ctx.fillStyle = 'rgb(0, 0, 0)';
-            ctx.fillText('loading...', pos.x, pos.y);
-            return;
-        }
+        //if (this.hasError) {
+        //	size = size || new Size(100, 100);
+        //	ctx.fillStyle = 'rgba(200, 0, 0, 0.2)';
+        //	ctx.fillRect(pos.x, pos.y, size.width, size.height);
+        //	ctx.fillStyle = 'rgb(0, 0, 0)';
+        //	ctx.fillText('error...', pos.x + 10, pos.y + 10);
+        //	return;
+        //}
+        //if (!this.hasLoaded) {
+        //	size = size || new Size(100, 100);
+        //	ctx.fillStyle = 'rgba(0, 0, 200, 0.2)';
+        //	ctx.fillRect(pos.x, pos.y, size.width, size.height);
+        //	ctx.fillStyle = 'rgb(0, 0, 0)';
+        //	ctx.fillText('loading...', pos.x, pos.y);
+        //	return;
+        //}
         if (size == null)
             ctx.drawImage(this.image, pos.x, pos.y, this.image.width, this.image.height);
         else
             ctx.drawImage(this.image, pos.x, pos.y, size.width, size.height);
-        if (size == null)
-            size = new Size(this.image.width, this.image.height);
-        ctx.strokeStyle = 'rgba(0, 200, 0, 1)';
-        ctx.strokeRect(pos.x, pos.y, size.width, size.height);
+        //if (size == null)
+        //	size = new Size(this.image.width, this.image.height);
+        //ctx.strokeStyle = 'rgba(0, 200, 0, 1)';
+        //ctx.strokeRect(pos.x, pos.y, size.width, size.height);
     };
     return Texture;
 })();
