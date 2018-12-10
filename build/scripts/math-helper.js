678910466dc7bf668fd2cf527c8bb1849804d31a
Jeremi Stadler <stadler.jeremi@gmail.com>
2018-12-10 08:26:44 +0100

    Remove build files

--- build/scripts/math-helper.js
+++
@@ -1,8 +0,0 @@
-var MathHelper = (function () {
-    function MathHelper() {
-    }
-    MathHelper.lerp = function (v0, v1, t) {
-        return v0 + t * (v1 - v0);
-    };
-    return MathHelper;
-})();
